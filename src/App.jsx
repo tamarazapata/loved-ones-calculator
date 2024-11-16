894a325b91937279c2dc4ed6cc5e55b1285f2d29
Tamara <tamara.zapatagomez@gmail.com>
2024-11-16 12:13:13 -0300

    added form and list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import  { useState } from 'react';
+import LovedOnesForm from './components/LovedOnesForm/LovedOnesForm';
+import LovedOnesList from './components/LovedOnesList/LovedOnesList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [lovedOnes, setLovedOnes] = useState([]);
+
+  const addLovedOne = (newLovedOne) => {
+    setLovedOnes([...lovedOnes, newLovedOne]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container d-flex align-items-start justify-content-between">
+      <div className="form-wrapper">
+        <LovedOnesForm onAddLovedOne={addLovedOne} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="list-wrapper">
+        <LovedOnesList lovedOnes={lovedOnes} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
